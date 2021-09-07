af0258ed6137101c6b67365dd36d03b9666a17f7
DukeManh <manhducdkcb@gmail.com>
2021-09-07 17:02:06 -0400

    Setting up eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    '@typescript-eslint/no-empty-function': 'off',
+    'func-names': 'off',
+    'no-console': 'off',
+    'prefer-arrow-callback': 'warn',
+    'no-unused-vars': 'warn',
+    'no-nested-ternary': 'off',
+  },
+};
