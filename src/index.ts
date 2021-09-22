05a3f8d6e5a47e43e83ec5e20b360106dda0ac3b
Kevan-Y <58233223+Kevan-Y@users.noreply.github.com>
2021-09-22 17:39:30 -0400

    Add markdown feature for h1,h2,p

--- src/index.ts
+++ src/index.ts
@@ -79,6 +79,61 @@ if (stylesheet) {
 }
 
 /**
+ * Process markdown for H1 (e.g: # headhing 1)
+ * @param data Data to be processed
+ * @return return array where h1 markdown is processed
+ */
+const processH1Markdown = (data: string[]): string[] => {
+  return data.map((text) => {
+    if (text.indexOf('# ') !== -1) {
+      if (text.substr(0, 2) === '# ') return `<h1>${text.substr(2)}</h1>`;
+    }
+    return `${text}`;
+  });
+};
+
+/**
+ * Process markdown for H2 (e.g: ## headhing 2)
+ * @param data Data to be processed
+ * @return return array where h2 markdown is processed
+ */
+const processH2Markdown = (data: string[]): string[] => {
+  return data.map((text) => {
+    if (text.indexOf('## ') !== -1) {
+      if (text.substr(0, 3) === '## ') return `<h2>${text.substr(3)}</h2>`;
+    }
+    return `${text}`;
+  });
+};
+
+/**
+ * Process markdown for P
+ * @param data Data to be processed
+ * @return return array where p markdown is processed
+ */
+const processPMarkdown = (data: string[]): string[] => {
+  return data.map((text) => {
+    if (text.substr(0, 1) !== '<' && text.substr(text.length - 1, 1) !== '>')
+      return `<p>${text}</p>`;
+    return `${text}`;
+  });
+};
+
+/**
+ * Process markdown
+ * It only support markdown for H1, H2, and P
+ * @param data Data to be processed
+ * @return return array of HTML
+ */
+const processMarkdown = (data: string[]): string[] => {
+  let treatedDataList: string[] = [];
+  treatedDataList = processH1Markdown(data);
+  treatedDataList = processH2Markdown(treatedDataList);
+  treatedDataList = processPMarkdown(treatedDataList);
+  return treatedDataList;
+};
+
+/**
  * Generate HTML mark up from .txt file
  * @param fileName File to be parsed
  * @param isIndex If false, add `Back to home` link to index page
@@ -86,17 +141,17 @@ if (stylesheet) {
  */
 const processFile = (filePath: string, isIndex: boolean): string => {
   const extension = path.extname(filePath).toLowerCase();
-  if (extension !== '.txt') {
+  if (extension !== '.txt' && extension !== '.md') {
     return '';
   }
 
+  const isMd = extension === '.md';
   const text = fs.readFileSync(filePath, 'utf-8');
 
   // title is before the first 2 blank lines of the text
   const titleAndContent = text.split(/\n\n\n/);
   let title = '';
   let content = '';
-
   if (titleAndContent.length >= 2) {
     [title] = titleAndContent;
     content = titleAndContent.slice(1).join('\n\n\n');
@@ -110,7 +165,7 @@ const processFile = (filePath: string, isIndex: boolean): string => {
                 <link rel="stylesheet" href="${
                   path.relative(path.dirname(filePath), input) || './'
                 }/index.css"> 
-                <title>${title || path.basename(filePath, '.txt')}</title>`;
+                <title>${title || path.basename(filePath, isMd ? '.md' : '.txt')}</title>`;
 
   const body = `
                 ${
@@ -121,10 +176,17 @@ const processFile = (filePath: string, isIndex: boolean): string => {
                     : ''
                 }
                 ${title ? `<h1 class="text-center">${title}</h1>` : ''}
-                ${content
-                  .split(/\r?\n\r?\n/)
-                  .map((para) => `<p>${para.replace(/\r?\n/, ' ')}</p>`)
-                  .join('\n')}
+                
+                ${
+                  isMd
+                    ? processMarkdown(
+                        content.split(/\r?\n\r?\n/).map((para) => `${para.replace(/\r?\n/g, ' ')}`)
+                      ).join('\n')
+                    : content
+                        .split(/\r?\n\r?\n/)
+                        .map((para) => `<p>${para.replace(/\r?\n/, ' ')}</p>`)
+                        .join('\n')
+                }
                   `;
 
   const markup = `<!DOCTYPE html>
@@ -156,7 +218,10 @@ const generateHTMLs = (pathName: string): string[] => {
         const markup = processFile(filePath, false);
         if (markup) {
           const relativeFolder = relative ? `/${path.relative(input, path.dirname(filePath))}` : '';
-          const dist = `${output}${relativeFolder}/${path.basename(filePath, '.txt')}.html`;
+          const dist = `${output}${relativeFolder}/${path.basename(
+            filePath,
+            path.extname(filePath).toLocaleLowerCase() === '.txt' ? '.txt' : '.md'
+          )}.html`;
 
           fs.ensureFileSync(dist);
           fs.writeFileSync(dist, markup, { flag: 'w' });
@@ -196,10 +261,17 @@ try {
 if (inputPath.isFile()) {
   const markup = processFile(input, true);
   if (!markup) {
-    logError('Input file must be .txt');
+    logError('Input file must be .txt or .md');
   }
 
-  fs.writeFileSync(`${output}/${path.basename(input, '.txt')}.html`, markup, { flag: 'w' });
+  fs.writeFileSync(
+    `${output}/${path.basename(
+      input,
+      path.extname(input).toLocaleLowerCase() === '.txt' ? '.txt' : '.md'
+    )}.html`,
+    markup,
+    { flag: 'w' }
+  );
 } else if (inputPath.isDirectory()) {
   const dists = generateHTMLs(input);
 
@@ -235,9 +307,3 @@ if (inputPath.isFile()) {
   logError(`${input}: No such file or directory`);
   process.exit(1);
 }
-
-const input_dir_entries: any[] = [];
-
-input_dir_entries.reduce((acc, file) => {
-  return acc;
-}, []);
