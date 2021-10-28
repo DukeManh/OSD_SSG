1c4bc09215cf9e1a689d3d8f0d28736e15b4a55c
DukeManh <manhducdkcb@gmail.com>
2021-10-28 01:46:01 -0400

    Use open-source Markdown parser

--- src/Parser.ts
+++ src/Parser.ts
@@ -1,3 +1,6 @@
+import MarkdownIt from 'markdown-it';
+
+const md = new MarkdownIt();
 export interface ParsedData {
   content: string;
   title: string | undefined;
@@ -12,35 +15,10 @@ class Parser {
 
   parseMarkdown(): ParsedData {
     let content = '';
-    let title: string | undefined;
+    const h1 = /^\s*#\s+(.+)$/gm;
+    const title = h1.exec(this.rawText)?.[1];
 
-    // Links could be either [name](href title) or [name](href)
-    const links = new RegExp(/\[(.*?)\]\((.+?)(?:\s"(.*?)")?\)/, 'gm');
-    content = this.rawText.replace(
-      links,
-      (match, p1, p2, p3) => `<a href="${p2}" ${p3 ? `title="${p3}"` : ''}>${p1}</a>`
-    );
-
-    const bold = new RegExp(/\*{2}(.+?)\*{2}/, 'gm');
-    content = content.replace(bold, '<strong>$1</strong>');
-
-    const italics = new RegExp(/\*{1}(.+?)\*{1}/, 'gm');
-    content = content.replace(italics, '<i>$1</i>');
-
-    content = content
-      .split(/(?:\r?\n)+/)
-      .map((paragraph) => {
-        const headings = new RegExp(/^\s*(#{1,6})\s+(.+)$/, 'gm');
-        if (headings.test(paragraph)) {
-          return paragraph.replace(headings, (match, hash, heading) => {
-            title = !(title === undefined) && hash.length === 1 ? heading : title;
-            const tag = `h${hash.length}`;
-            return `<${tag}>${heading}</${tag}>`;
-          });
-        }
-        return `<p>${paragraph}</p>`;
-      })
-      .join('\n');
+    content = md.render(this.rawText);
 
     return { content, title };
   }
