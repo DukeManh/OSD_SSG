89a23a64bc62813140b78a858500a97260cfdcb7
DukeManh <manhducdkcb@gmail.com>
2021-10-28 01:55:46 -0400

    Code block syntax highlighting

--- src/Parser.ts
+++ src/Parser.ts
@@ -1,6 +1,20 @@
 import MarkdownIt from 'markdown-it';
+import hljs from 'highlight.js';
 
-const md = new MarkdownIt();
+const md = new MarkdownIt({
+  highlight(str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return `<pre class="hljs"><code>${
+          hljs.highlight(str, { language: lang, ignoreIllegals: true }).value
+        }</code></pre>`;
+      } catch (__) {}
+    }
+
+    return `<pre class="hljs"><code>${md.utils.escapeHtml(str)}</code></pre>`;
+  },
+});
+
 export interface ParsedData {
   content: string;
   title: string | undefined;
